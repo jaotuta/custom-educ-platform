84aeb82dec8310e96cf999545bd43f18450a99c5
João Lucas Cruz <63518059+jaotuta@users.noreply.github.com>
2024-12-12 21:05:27 +0000

    adiciona scripts lint

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <body><h1>Hello World!!!</h1> <h2>Atualização</h2></body>
+  return (
+    <body>
+      <h1>Hello World!!!</h1> <h2>Atualização</h2>
+    </body>
+  );
 }
 
 export default Home;
